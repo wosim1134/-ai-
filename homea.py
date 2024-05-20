80f44a6d63bf1667bab15a83f6ba431097ccecfd
wosim1134 <pubglnar@gmail.com>
2024-05-21 08:42:28 +0900

    kk

--- homea.py
+++ homea.py
@@ -1,1 +1,13 @@
-print('a')
+a = int(input())
+b = [[i for i in input()] for j in range(a)]
+
+for i in range(a):
+    b[i].append(' ')
+
+print(b)
+
+for i in range(len(b)):
+    for j in range(len(b[i])-1):
+        if b[i][j] == b[i][j-1]:
+            del b[i][j]
+print(b)
