69d1f0bef0cde0acdc600c8f77b72b852d1e5a61
wosim1134 <pubglnar@gmail.com>
2024-05-27 23:28:01 +0900

    Update homea.py

--- homea.py
+++ homea.py
@@ -1,32 +1,6 @@
-<<<<<<< Updated upstream
-a = int(input())
-b = []
-c = 0
-for i in range(a):
-    b.append(input())
-
-
-
-print(b)
-
-for i in range(len(b)):
-    d = [0 for p in range(len(b[i]))]
-
-    for j in range(len(b[i])-2):
-
-        for k in range(j+2, len(b[i])):
-
-            if b[i][j] == b[i][k]:
-
-                if b[i][j] != b[i][j+1]:
-                    
-                    c += 1
-=======
 a = {
-    'a', 12,
-    'b', 23
+    'q' : 12,
+    's' : 23
 }
 
-z = a.get('a', 'no')
-print(z)
->>>>>>> Stashed changes
+print(a.get(12))
