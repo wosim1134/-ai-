9b4e356f7807fc62443554ec966e397ca211b15c
wosim1134 <pubglnar@gmail.com>
2024-05-28 09:44:24 +0900

    Update homea.py

--- homea.py
+++ homea.py
@@ -1,3 +1,11 @@
 a, b = input().split()
+b = int(b)
+c = 0
 
-print(ord('A'))
+for i in range(1, len(a)+1):
+    if '0' <= a[-i] <= '9':
+        c += (ord(a[-i]) - ord('0')) * b ** (i - 1)
+    else:
+        c += (ord(a[-i]) - 55) * b ** (i - 1)
+
+print(c)
