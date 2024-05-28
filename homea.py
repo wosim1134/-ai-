18bc744f5dafb549c0cfc306c480313cbf8373f8
wosim1134 <pubglnar@gmail.com>
2024-05-28 10:38:29 +0900

    hgtr

--- homea.py
+++ homea.py
@@ -1,8 +1,15 @@
 a, b = map(int, input().split())
 c = 0
-d = 0
+d = []
 
 while True:
     c = a % b
     a = a // b
     if 0 <= c <= 9:
+        d.insert(0, c)
+    else:
+        d.insert(0, chr(c+55))
+    if a == 0:
+        for i in range(len(d)):
+            print(d[i], end = '')
+        break
