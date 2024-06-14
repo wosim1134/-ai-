6f10bbc2aec7fe35f7d54ce3d179d7676b84192d
wosim1134 <pubglnar@gmail.com>
2024-06-14 16:04:28 +0900

    Update homea.py

--- homea.py
+++ homea.py
@@ -1,6 +1,18 @@
-import math
-A, B, V = map(int, input().split())
-C = A - B
+while True:
+    a = int(input())
+    if a == -1:
+        break
+    b = 0
+    c = []
+    if a > 0:
+        for i in range(1, a):
+            if a % i == 0:
+                b += i
+                c.append(i)
 
-m = math.ceil((V-A)/C +1)
-print(m)
+        if a == b and a > 0:
+            print(f'{a} = 1', end = '')
+        for i in range(1, len(c)):
+            print(f' + {c[i]}', end = '')
+    else:
+        print(f'{a} is NOT perfect.')
