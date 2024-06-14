b8165c087bae294cfd240c600cd7c87c98ac2664
wosim1134 <pubglnar@gmail.com>
2024-06-14 15:06:08 +0900

    Update homea.py

--- homea.py
+++ homea.py
@@ -1,10 +1,6 @@
+import math
 A, B, V = map(int, input().split())
-
 C = A - B
-# M-1 * C + A > v
-# 4-1   1   2   5
-# 2-1   4   5   6
-C * V - A = M-1
-1   5   2   4-1
 
-M-1 = 
+m = math.ceil((V-A)/C +1)
+print(m)
