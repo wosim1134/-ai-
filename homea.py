5c4f5f69d45415a7dce1580e0ab4a6695fbf79fb
wosim1134 <pubglnar@gmail.com>
2024-06-21 08:38:02 +0900

    Update homea.py

--- homea.py
+++ homea.py
@@ -1,17 +1,17 @@
-import sys
-input = sys.stdin.readline
-
-while True:
-    a = list(map(int, input().split()))
-    s = sum(a)
-    if a[0] == a[1] == a[2] == 0:
-        break
-    if (s-max(a)) > max(a):
-        if a[0] == a[1] == a[2]:
-            print('Equilateral')
-        elif a[0] == a[1] or a[1] == a[2] or a[0] == a[2]:
-            print('Isosceles')
+def aq(a):
+    z = []
+    for i in a:
+        if type(i) == list:
+            z += aq(i)
         else:
-            print('Scalene')
-    else:
-        print('Invalid')
+            z.append(i)
+    return z
+
+
+
+
+
+
+
+a = [1, [2, 3], [[4, 5], 6], [[[7]], 8, 9], [10]]
+print(aq(a))
