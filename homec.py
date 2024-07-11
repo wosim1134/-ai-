17f3a3483b033a15973f914f5301c7f0ade33c65
wosim1134 <pubglnar@gmail.com>
2024-07-11 10:18:31 +0900

    Update homec.py

--- homec.py
+++ homec.py
@@ -1,10 +1,21 @@
-a, b = map(int, input().split())
+a0, a1 = map(int, input().split())
 c = int(input())
 d = int(input())
 
+if (a0 - c) * d + a1 <= 0 and a0 <= c:
+    print(1)
+else:
+    print(0)
 
+
+# a0n + a1 <= cn
+
+# (a0 - c)n + a1 <= 0, n >= d
 #
-# an + b <= cn
-# (a - c)n + b <= 0, n >= d
-# c == a, b == 0 d = X
-# c > a, d >= b
+# c == a0:  a1 == 0, d = X
+#
+# c > a0:  d >= a1
+#
+#
+#
+#
