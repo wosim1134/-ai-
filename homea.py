ac1753d3de9a4459bb2f9a5f717c32b96f2a683e
wosim1134 <pubglnar@gmail.com>
2024-07-13 19:37:05 +0900

    Update homea.py

--- homea.py
+++ homea.py
@@ -1,18 +1,1 @@
-while True:
-    a = int(input())
-    if a == -1:
-        break
-    b = 0
-    c = []
-    if a > 0:
-        for i in range(1, a):
-            if a % i == 0:
-                b += i
-                c.append(i)
-
-        if a == b and a > 0:
-            print(f'{a} = 1', end = '')
-        for i in range(1, len(c)):
-            print(f' + {c[i]}', end = '')
-    else:
-        print(f'{a} is NOT perfect.')
+a = int(input())
