16f788595929d70fe2b92ff4fe1c614e12f2dda0
wosim1134 <pubglnar@gmail.com>
2024-07-13 20:16:36 +0900

    Update homec.py

--- homec.py
+++ homec.py
@@ -1,28 +1,7 @@
 import sys
 input = sys.stdin.readline
-a = input().strip()
-b = len(a)
-a = int(a)
-c = a - b*9
-d = []
-e = ''
-g = []
+a, b, c, d, e, f = map(int, input().split())
 
-for i in range(c, a+1):
-    if i > 0:
-        for j in range(len(str(i))):
-            d.append(int(str(i)[j]))
-        f = i + sum(d)
-        if a == f:
-            d = list(map(str, d))
-            for k in d:
-                e += k
-            e = int(e)
-            g.append(e)
-        d.clear()
-        e = ''
-
-if g == []:
-    print(0)
-else:
-    print(min(g))
+y = (c*d-a*f) / (b*d-a*e)
+x = (-b*y+c)/a
+print(int(x), int(y))
