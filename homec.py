df2b0b771e48df5ee4664e8f31885c430dd46d2a
wosim1134 <pubglnar@gmail.com>
2024-07-14 20:30:48 +0900

    Update homec.py

--- homec.py
+++ homec.py
@@ -1,6 +1,21 @@
 import sys
 input = sys.stdin.readline
 n, m = map(int, input().split())
-
 a = [input().rstrip() for i in range(n)]
-print(a)
+b = ['BWBWBWBW', 'WBWBWBWB'] * 4
+c = ['WBWBWBWB', 'BWBWBWBW'] * 4
+d = e = 0
+h = 100
+
+for i in range(n-7):
+    for j in range(m-7):
+        d = e = 0
+        for k in range(8):
+            for l in range(8):
+                if a[k+i][l+j] != b[k][l]:
+                    d += 1
+                if a[k+i][l+j] != c[k][l]:
+                    e += 1
+        h = min(d, e, h)
+
+print(h)
