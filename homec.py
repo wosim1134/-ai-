e86fa29233332a0b9f10f6da4717abe195e989e4
wosim1134 <pubglnar@gmail.com>
2024-07-29 11:36:02 +0900

    Update homec.py

--- homec.py
+++ homec.py
@@ -1,6 +1,3 @@
-a = int(input())
-b = [tuple(map(int, input().split())) for i in range(a)]
-
-c = sorted(b, key=lambda point: (point[0], point[1]))
-for i in c:
-    print(f'{i[0]} {i[1]}')
+import sys
+input = sys.stdin.readline
+# input => input()
