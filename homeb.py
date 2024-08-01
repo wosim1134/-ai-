c83841a4ee95a3fbf786a27c8b95b005cb403859
wosim1134 <pubglnar@gmail.com>
2024-08-01 12:39:35 +0900

    dsa

--- homeb.py
+++ homeb.py
@@ -1,10 +1,6 @@
 a = int(input())
-b = [list(map(int, input().split())) for i in range(a)]
-
-for i in range(a-1):
-    for j in range(i, a):
-        if b[i][0] >= b[j][0]:
-            b[i], b[j] = b[j], b[i]
-        if b[i][0] == b[j][0] and b[i][1] > b[j][1]:
-            b[i], b[j] = b[j], b[i]
+b = [tuple(map(int, input().split())) for i in range(a)]
 print(b)
+c = sorted(b, key=lambda point: (point[0], point[1]))
+for i in c:
+    print(f'{i[0]} {i[1]}')
--- homec.py
+++ homec.py
@@ -1,2 +1,14 @@
 import sys
 input = sys.stdin.readline
+
+a = int(input())
+b = [list(input().split()) for i in range(a)]
+
+for i in range(a):
+    b[i][0] = int(b[i][0])
+    b[i].append(i)
+
+b.sort(key=lambda x: (x[0], x[2]))
+
+for i in b:
+    print(f'{i[0]} {i[1]}')
