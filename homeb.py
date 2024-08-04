92d1372e8bf51e3b1c32ef7b066a9d476a85099f
wosim1134 <pubglnar@gmail.com>
2024-08-04 17:02:21 +0900

    fds

--- homeb.py
+++ homeb.py
@@ -1,29 +1,12 @@
 import sys
 input = sys.stdin.readline
 
-Num = []
-Num_dict = {}
+a, b = map(int, input().split())
+c = 0
+a1 = [input().strip() for i in range(a)]
+b1 = [input().strip() for i in range(b)]
 
-Count1 = int(input())
-Number1 = list(map(int, input().split()))
-Count2 = int(input())
-Number2 = list(map(int, input().split()))
-Number_1 = sorted(set(Number1))
-Number_2 = sorted(set(Number2))
-
-a = 0
-b = 0
-
-print(Number1)
-print(Number2)
-print(Number_1)
-print(Number_2)
-
-while a <= Count1 and b <= Count2:
-    if Number_1[a] == Number_2[b]:
-        Num_dict[Number_2[b]] = 1
-    
-
-    a += 1
-    b += 1
-print(Num_dict)
+for i in b1:
+    if i in a1:
+        c += 1
+print(c)
--- homec.py
+++ homec.py
@@ -1,3 +1,13 @@
-dict1 = {}
-dict1[0] = 12
-print(dict1)
+import sys
+input = sys.stdin.readline
+
+n = int(input().strip())
+coords = list(map(int, input().split()))
+
+sorted_coords = sorted(set(coords))
+
+coord_dict = {value: index for index, value in enumerate(sorted_coords)}
+
+compressed_coords = [coord_dict[x] for x in coords]
+
+print(' '.join(map(str, compressed_coords)))
