763c054d1b8fb4a85906119c5794448fe1d6de74
wosim1134 <pubglnar@gmail.com>
2024-08-04 18:25:43 +0900

    dsadsf

--- homeb.py
+++ homeb.py
@@ -1,23 +1,29 @@
 import sys
 input = sys.stdin.readline
 
+# 포켓몬 수와 질문 수 입력 받기
 a, b = map(int, input().split())
-pokemon = {}
-c = []
-for i in range(a):
-    pokemon[i+1] = input().strip()
 
+# 포켓몬 딕셔너리 초기화
+pokemon = {}
+for i in range(a):
+    pokemon[i + 1] = input().strip()
+
+# 질문 리스트 초기화
+queries = []
 for i in range(b):
     q = input().strip()
     try:
-        c.append(int(q))
+        queries.append(int(q))
     except:
-        c.append(q)
+        queries.append(q)
 
-w = list(pokemon.values())
+# 포켓몬 이름 리스트 생성
+pokemon_names = list(pokemon.values())
 
-for i in c:
-    if type(i) == int:
-        print(pokemon[i])
-    elif type(i) == str:
-        print(w.index(i) + 1)
+# 질문 처리 및 출력
+for q in queries:
+    if isinstance(q, int):
+        print(pokemon[q])
+    else:
+        print(pokemon_names.index(q) + 1)
