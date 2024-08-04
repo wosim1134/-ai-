ef004648a61da3f94a6f20468d94dbf9b421db92
wosim1134 <pubglnar@gmail.com>
2024-08-04 17:37:10 +0900

    Update homeb.py

--- homeb.py
+++ homeb.py
@@ -1,12 +1,18 @@
 import sys
 input = sys.stdin.readline
 
-a, b = map(int, input().split())
-c = 0
-a1 = [input().strip() for i in range(a)]
-b1 = [input().strip() for i in range(b)]
+a = int(input())
+dict1 = {}
+d = []
+for i in range(a):
+    b, c = input().split()
+    dict1[b] = c
+    if dict1[b] == 'leave':
+        del dict1[b]
 
-for i in b1:
-    if i in a1:
-        c += 1
-print(c)
+for i in dict1.items():
+    d.append(i)
+d.sort(key=lambda x: x[0])
+
+for i in reversed(d):
+    print(i[0])
