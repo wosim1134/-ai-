b7be13ab698420c736ce2b1ab64c686bbfd0a7c3
wosim1134 <pubglnar@gmail.com>
2024-08-07 15:47:25 +0900

    Update homeb.py

--- homeb.py
+++ homeb.py
@@ -1,18 +1,3 @@
 import sys
 input = sys.stdin.readline
 
-a, b = map(int, input().split())
-
-pokemon = {}
-name_to_index = {}
-for i in range(a):
-    name = input().strip()
-    pokemon[i + 1] = name
-    name_to_index[name] = i + 1
-
-for _ in range(b):
-    q = input().strip()
-    if q.isdigit():
-        print(pokemon[int(q)])
-    else:
-        print(name_to_index[q])
