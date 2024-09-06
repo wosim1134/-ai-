ccc8d0301b2a8eb5fcc27679bb9a50abca0dbe29
wosim1134 <pubglnar@gmail.com>
2024-09-06 21:20:56 +0900

    소수

--- homeb.py
+++ homeb.py
@@ -1,4 +1,20 @@
-import sys
-input = sys.stdin.readline
+a = 1
+b = [2]
+c = int(input())
+d = 0
+while True:
+    a += 1
+    d = 0
+    if c == a:
+        break
 
-a = int(input())
+    for i in range(len(b)):
+        if a % b[i] == 0:
+            break
+        else:
+            d += 1
+            if d == len(b):
+                b.append(a)
+
+
+print(b)
