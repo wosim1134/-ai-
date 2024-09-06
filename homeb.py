5cdd79deda3fbc074cd8779e06aa561438854860
wosim1134 <pubglnar@gmail.com>
2024-09-06 22:07:05 +0900

    소수 n(2~9)진법으로 바꾸기

--- homeb.py
+++ homeb.py
@@ -1,13 +1,13 @@
 a = 1
 b = [2]
-c = int(input())
+c = int(input()) # 소수 구하기
 d = 0
+
 while True:
     a += 1
     d = 0
     if c == a:
         break
-
     for i in range(len(b)):
         if a % b[i] == 0:
             break
@@ -16,5 +16,23 @@ while True:
             if d == len(b):
                 b.append(a)
 
-
 print(b)
+
+f = int(input()) # 진법
+g = []
+h = []
+for i in b:
+    while True:
+        if i // f == 0:
+            g.insert(0, i)
+            break
+        g.insert(0, i % f)
+        i = i // f
+    g = list(map(str, g))
+    g = ''.join(g)
+    g = int(g)
+    h.append(g)
+    g = []
+
+
+print(h)
